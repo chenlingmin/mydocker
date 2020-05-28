a5db33c61b5d890a93fb6eb340a015cf516c1ed3
陈灵敏 <chenlingmin@mem853.com>
2020-05-28 11:42:34 +0800

    -

--- nsenter/nsenter.go
+++ nsenter/nsenter.go
@@ -1,29 +1,30 @@
 package nsenter
 
 /*
+#define _GNU_SOURCE
+#include <unistd.h>
 #include <errno.h>
 #include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
-#include <unistd.h>
 
 __attribute__((constructor)) void enter_namespace(void) {
 	char *mydocker_pid;
 	mydocker_pid = getenv("mydocker_pid");
 	if (mydocker_pid) {
-		fprintf(stdout, "got mydocker_pid=%s\n", mydocker_pid);
+		//fprintf(stdout, "got mydocker_pid=%s\n", mydocker_pid);
 	} else {
-		fprintf(stdout, "missing mydocker_pid env skip nsenter");
+		//fprintf(stdout, "missing mydocker_pid env skip nsenter");
 		return;
 	}
 	char *mydocker_cmd;
 	mydocker_cmd = getenv("mydocker_cmd");
 	if (mydocker_cmd) {
-		fprintf(stdout, "got mydocker_cmd=%s\n", mydocker_cmd);
+		//fprintf(stdout, "got mydocker_cmd=%s\n", mydocker_cmd);
 	} else {
-		fprintf(stdout, "missing mydocker_cmd env skip nsenter");
+		//fprintf(stdout, "missing mydocker_cmd env skip nsenter");
 		return;
 	}
 	int i;
@@ -35,9 +36,9 @@ __attribute__((constructor)) void enter_namespace(void) {
 		int fd = open(nspath, O_RDONLY);
 
 		if (setns(fd, 0) == -1) {
-			fprintf(stderr, "setns on %s namespace failed: %s\n", namespaces[i], strerror(errno));
+			//fprintf(stderr, "setns on %s namespace failed: %s\n", namespaces[i], strerror(errno));
 		} else {
-			fprintf(stdout, "setns on %s namespace succeeded\n", namespaces[i]);
+			//fprintf(stdout, "setns on %s namespace succeeded\n", namespaces[i]);
 		}
 		close(fd);
 	}
